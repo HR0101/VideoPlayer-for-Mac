e0ed6d6e121ca1956bbcd90d133f9b489d93116b
HR0101 <hararyuto0101@gmail.com>
2025-06-18 14:17:09 +0900

    Initial Commit

---
+++ VideoPlayer for Mac/VideoPlayer_for_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_for_MacApp.swift
+//  VideoPlayer for Mac
+//
+//  Created by hara ryuto   on 2025/06/18.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayer_for_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayer for MacTests/VideoPlayer_for_MacTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_for_MacTests.swift
+//  VideoPlayer for MacTests
+//
+//  Created by hara ryuto   on 2025/06/18.
+//
+
+import Testing
+@testable import VideoPlayer_for_Mac
+
+struct VideoPlayer_for_MacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
